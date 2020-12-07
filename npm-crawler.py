bd79bb5a10d8252445c126829228399bf076c6a6
Fangchen Li <fangchen.li@outlook.com>
2020-12-07 16:26:01 -0600

    data

--- npm-crawler.py
+++ npm-crawler.py
@@ -1,7 +1,16 @@
+from json import load, dump
 from math import ceil
+import logging
 
 from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import ConnectionError
+
+
+package_per_page = 36
+dep_selector = "#dependencies > ul:nth-child(2) > li > a"
+logging.basicConfig(filename="request.log", level=logging.DEBUG)
+s = requests.Session()
 
 
 def npm_top_dep_format(offset: int):
@@ -17,7 +26,7 @@ def npm_depended_format(module: str, offset: int):
 
 
 def npm_get_most_depended_upon_packages(limit: int):
-    package_per_page = 36
+
     num_pages = ceil(limit / package_per_page)
     packages = []
     for i in range(num_pages):
@@ -33,18 +42,17 @@ def npm_get_most_depended_upon_packages(limit: int):
     return packages[:limit]
 
 
-def npm_get_num_dependents(package: str):
+def get_num_dependencies(package: str):
     url = npm_package_format(package)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     tags = soup.find_all("span", class_="c3fc8940")
-    num_dependents = tags[1].text
+    num_dependents = tags[0].text
     num_dependents = "".join(c for c in num_dependents if c.isdigit())
     return int(num_dependents)
 
 
 def npm_get_dependents(package: str, num_dependents: int):
-    package_per_page = 36
     num_pages = ceil(num_dependents / package_per_page)
     packages = []
     for i in range(num_pages):
@@ -61,12 +69,96 @@ def npm_get_dependents(package: str, num_dependents: int):
     return packages
 
 
-# num_package = 100
-# package_list = npm_get_most_depended_upon_packages(num_package)
+def import_packages_recursive(name, deps_dict, max_depth=10, depth=0):
+    if name in deps_dict or depth > max_depth:
+        return
 
-tmp_package = "lodash"
-num_deps = npm_get_num_dependents(tmp_package)
-print(num_deps)
-deps_list = npm_get_dependents(tmp_package, num_deps)
-print(deps_list)
-print(len(deps_list))
+    url = npm_package_format(name) + "?activeTab=dependencies"
+
+    try:
+        page = requests.get(
+            url="https://app.scrapingbee.com/api/v1/",
+            params={
+                "api_key": "W89DTQW6MCTY0F94F6BYTC7OZ564UQRQV5SUJM75NIUN7WUK69AIUG1H4POV2IO8OBPI159AOES9K4P9",
+                "url": url,
+                "premium_proxy": "true",
+            },
+        )
+    except ConnectionError:
+        logging.debug(name)
+        return
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # parse number of dependencies
+    tags = soup.find_all("span", class_="c3fc8940")
+    if len(tags) == 0:
+        logging.debug(name)
+        return
+
+    num_dependency = tags[0].text
+    num_dependency = "".join(c for c in num_dependency if c.isdigit())
+    num_dependency = int(num_dependency)
+
+    # parse dependencies
+    dependencies = soup.select(dep_selector)
+    dependencies_list = [dep.text for dep in dependencies]
+
+    if num_dependency != len(dependencies_list):
+        print(f'request package {name} failed')
+        print(num_dependency, len(dependencies_list))
+
+    # print(f'package {name} has dependencies {dependencies_list}')
+
+    deps_dict[name] = dependencies_list
+
+    # recursively call itself to import all dependencies
+    for dep in dependencies_list:
+        import_packages_recursive(dep, deps_dict, max_depth=max_depth, depth=depth + 1)
+
+
+#
+
+# # num_package = 100
+# # package_list = npm_get_most_depended_upon_packages(num_package)
+#
+#
+# tmp_package = "request"
+# num_deps = get_num_dependencies(tmp_package)
+# print(num_deps)
+# deps_list = npm_get_dependents(tmp_package, num_deps)
+# print(deps_list)
+# print(len(deps_list))
+
+# tmp_package = "@oclif/plugin-help"
+# result = {}
+# import_packages_recursive(tmp_package, result)
+# print(result)
+
+# @oclif/plugin-help
+
+# with open("data/wallet.json") as file:
+#     data = load(file)
+#
+# counter = 0
+# result = {}
+# for _, dep_list in data.items():
+#     for dependency in dep_list:
+#         import_packages_recursive(dependency, result)
+#     counter += 1
+#     print(f'{counter} packages have finished, {len(data)} total.')
+#
+# with open(f"data/wallet_complete.json", "w") as file:
+#     dump(result, file, indent=4)
+
+failed_package = "@oclif/plugin-help"
+result = {}
+import_packages_recursive(failed_package, result)
+
+with open("data/wallet_complete.json") as file:
+    data = load(file)
+
+data.update(result)
+
+with open(f"data/wallet_complete_with_failed.json", "w") as file:
+    dump(data, file, indent=4)
